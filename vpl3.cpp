962c21cf7dffe8e34205fe3c7bd6d9059f7ccec0
mateus-mqs <mateusmarques447@gmail.com>
2021-05-22 20:31:36 -0300

    adding vpl3

---
+++ vpl3.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main(){
+
+    // 1) Declare uma variável do tipo inteiro e atribua o valor '5'.
+    int var = 5;
+
+    // 2) Declare um vetor de inteiros e inicialize com valores de 0 a 9.
+    vector<int> vec = {0,1,2,3,4,5,6,7,8,9};
+
+    // 3) Declare um ponteiro para inteiros e inicialize com valor nulo.
+    
+
+    // 4) Imprima o ENDEREÇO da variável declarada em (1)
+    
+
+    // 5) Imprima o VALOR da variável declarada em (1)
+    
+
+    // 6) Imprima o ENDEREÇO da variável declarada em (2)
+    
+
+    // 7) Imprima o ENDEREÇO da variável declarada em (3)
+    
+    
+    // 8) Imprima o VALOR da variável declarada em (3)
+    
+
+    // 9) Atribua o ENDEREÇO da variável declarada em (1) à variável declarada em (3)
+    
+
+    // 10) Imprima o VALOR da variável declarada em (3)
+    
+
+    // 11) Imprima o VALOR guardado no ENDEREÇO apontado por (3)
+    
+
+    // 12) Imprima o resultado da comparação do ENDEREÇO de (1) e do VALOR de (3)
+    
+
+    // 13) Coloque o VALOR '10' no ENDEREÇO apontado por (3)
+    
+
+    // 14) Imprima o VALOR da variável declarada em (1)
+    
+
+    // 15) Atribua o ENDEREÇO da variável (2) à variável declarada em (3)
+    
+
+    // 16) Imprima o VALOR da variável declarada em (3)
+    
+
+    // 17) Imprima o VALOR guardado no ENDEREÇO apontado por (3)
+    
+
+    // 18) Atribua o ENDEREÇO da primeira posição de (2) à variável declarada em (3)
+    
+
+    // 19) Imprima o VALOR da variável declarada em (3)
+    
+
+    // 20) Imprima o VALOR guardado no ENDEREÇO apontado por (3)
+    
+
+    // 21) Multiplique todos os valores do vetor declarado em (2) por '10', porém manipulando apenas a variável (3)
+    
+
+    // 22) Imprima os elementos de (2) a partir variável do vetor utilizando a notação [] (colchetes)
+    
+
+    // 23) Imprima os elementos de (2) a partir variável do vetor utilizando a notação ponteiro/deslocamento
+    
+
+    // 24) Imprima os elementos de (2) manipulando apenas a variável (3)
+    
+
+    // 25) Imprima o resultado da comparação do VALORES armazenado na quinta posição de (2) e no ponteiro (3) incrementado de 4
+    
+
+    // 26) Atribua o ENDEREÇO da quinta posição de (2) à variável declarada em (3)
+    
+
+    // 27) Imprima o VALOR guardado no ENDEREÇO apontado por (3)
+    
+
+    // 28) Imprima o VALOR guardado no ENDEREÇO do ponteiro (3) decrementado de 4
+    
+    return 0;
+}
